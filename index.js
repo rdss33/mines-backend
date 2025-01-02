f8f5805a5b9b5edce626b9a552466373205abbc5
rdss33 <raphaeldssoares@gmail.com>
2025-01-02 11:37:29 -0400

    Dynamic Port
    Heroku needs to assign a port through env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 //#region Backend setup
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 //#endregion
